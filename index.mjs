2db4559f1ab6f4c2594533c51dbca2208e16fc9d
Vidar Ramdal <vramdal@gmail.com>
2022-11-29 16:00:24 +0100

    Første versjon

---
+++ index.mjs
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import httpServer from "http-server";
+import chalk from 'chalk';
+
+import {program} from "commander";
+import * as path from "path";
+
+const logger = {info: console.log, error: console.error};
+
+program.option("-p, --port", "port to listen on", "8080");
+program.option("-d, --directory", "directory to serve", ".");
+
+process.on('SIGINT', function () {
+    logger.info(chalk.red('http-server stopped.'));
+    process.exit();
+});
+
+process.on('SIGTERM', function () {
+    logger.info(chalk.red('http-server stopped.'));
+    process.exit();
+});
+
+const server = httpServer.createServer({
+    root: program.getOptionValue("directory"),
+    showDir: true,
+    showDotfiles: true,
+    cors: true,
+    corsHeaders: "authorization"
+});
+
+server.listen(program.getOptionValue("port"), () => {
+    console.log(`Server ${path.resolve(program.getOptionValue("directory"))} på port ${program.getOptionValue("port")}`);
+});
